dfcf70f92631b24e35efc3f13a28fbed42c0304f
Marwa <marwakhalid558@gmail.com>
2023-12-15 22:25:07 +0500

    Update ProductsTable.jsx

--- src/components/Table/ProductsTable.jsx
+++ src/components/Table/ProductsTable.jsx
@@ -169,7 +169,7 @@ const ProductsTable = () => {
     rowsPerPageOptions: [10, 25, 50],
     customToolbar: () => {
       return ( 
-          <button id="add" onClick={handleOpenModal}>Add</button>          
+          <button class="addButton" id="addButton" onClick={handleOpenModal}>Add</button>          
       );
     },
   };
